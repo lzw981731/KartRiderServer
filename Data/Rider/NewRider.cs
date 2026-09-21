9071f1f89f86ed89444afe01e0d2e3c71fe36671
lzw981731 <lzw981731@gmail.com>
2026-09-22 00:21:14 +0800

    Purchase mode: disable full-item broadcast in LoadItemData (NewKart1/Items/Parts), only send owned items

--- Data/Rider/NewRider.cs
+++ Data/Rider/NewRider.cs
@@ -23,20 +23,23 @@ namespace RiderData
             KartExcData.Parts_ExcData(Parent, Nickname);
             KartExcData.Level12_ExcData(Parent, Nickname);
             KartExcData.Parts12_ExcData(Parent, Nickname);
-            NewRider.XUniquePartsData(Parent, Nickname);
-            NewRider.XLegendPartsData(Parent, Nickname);
-            NewRider.XRarePartsData(Parent, Nickname);
-            NewRider.XNormalPartsData(Parent, Nickname);
-            NewRider.V1UniquePartsData(Parent, Nickname);
-            NewRider.V1LegendPartsData(Parent, Nickname);
-            NewRider.V1RarePartsData(Parent, Nickname);
-            NewRider.V1NormalPartsData(Parent, Nickname);
-            NewRider.partsEngine12(Parent, Nickname);
-            NewRider.partsHandle12(Parent, Nickname);
-            NewRider.partsWheel12(Parent, Nickname);
-            NewRider.partsBooster12(Parent, Nickname);
-            NewRider.Items(Parent, Nickname);
-            NewRider.NewKart1(Parent);
+            // 购买模式：以下为全量下发函数（把服务器内存中所有物品/车辆/零件发给客户端），
+            // 会导致客户端"我的物品"显示所有车辆和道具。改为拥有制后全部停用，
+            // 仅由 NewKart2(已购车辆)/NewItem(已购道具) 下发玩家实际拥有的内容。
+            //NewRider.XUniquePartsData(Parent, Nickname);
+            //NewRider.XLegendPartsData(Parent, Nickname);
+            //NewRider.XRarePartsData(Parent, Nickname);
+            //NewRider.XNormalPartsData(Parent, Nickname);
+            //NewRider.V1UniquePartsData(Parent, Nickname);
+            //NewRider.V1LegendPartsData(Parent, Nickname);
+            //NewRider.V1RarePartsData(Parent, Nickname);
+            //NewRider.V1NormalPartsData(Parent, Nickname);
+            //NewRider.partsEngine12(Parent, Nickname);
+            //NewRider.partsHandle12(Parent, Nickname);
+            //NewRider.partsWheel12(Parent, Nickname);
+            //NewRider.partsBooster12(Parent, Nickname);
+            //NewRider.Items(Parent, Nickname);
+            //NewRider.NewKart1(Parent);
             NewRider.NewKart2(Parent);
             NewRider.NewItem(Parent);
             NewRider.NewRiderData(Parent);//라이더 인식
